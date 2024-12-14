35a0264c623495e28c9beb766c829896be9f11ca
Andreas Larssamils <larssamils.andreas@gmail.com>
2024-12-14 20:56:09 +0100

    fotocollage migration

---
+++ app/page.js
@@ -0,0 +1,20 @@
+import { MasonryPhotoAlbum } from 'react-photo-album';
+import 'react-photo-album/masonry.css';
+
+import photos from '@/public/photos';
+
+export default function Home() {
+  return (
+    <>
+      <h1>Photo Album</h1>
+      <p>Här är bilder utan inbördesordning</p>
+      <div className="container">
+        <MasonryPhotoAlbum
+          photos={photos}
+          column={3}
+        />
+      </div>
+    </>
+  );
+}
+
